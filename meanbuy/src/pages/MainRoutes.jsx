d69d34f434bd7d4ec9712597ab383c27146ddfe6
setu bhaskar jha <sbhaskar1998@gmail.com>
2022-11-10 15:15:20 +0530

    added electronics page, product page

--- meanbuy/src/pages/MainRoutes.jsx
+++ meanbuy/src/pages/MainRoutes.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Login from './Login'
-import SignUp from './SignUp'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
 const MainRoutes = () => {
   return (
-    <Routes >
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/SignUp' element={<SignUp />}></Route>
-        <Route path="*" element={{}}/>
+    <Routes>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/SignUp" element={<SignUp />}></Route>
+      <Route path="*" element={<div>Page Not Found!</div>} />
     </Routes>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;

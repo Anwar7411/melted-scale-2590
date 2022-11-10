bb50512c76bdbd8a22fdbe6b3cc2ff8dd9a8baae
Anwar Basha <bashaa7411@gmail.com>
2022-11-10 10:26:18 +0530

    renamed

--- meanbuy/src/pages/mainRoutes.jsx
+++ meanbuy/src/pages/mainRoutes.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Login from './Login'
-import SignUp from './SignUp'
+
 const MainRoutes = () => {
   return (
-    <Routes >
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/SignUp' element={<SignUp />}></Route>
-        <Route path="*" element={{}}/>
-    </Routes>
+    <div>mainRoutes</div>
   )
 }
 

8a366a6ac35101d579e4ecbc84d6ef23b0913611
Anwar Basha <bashaa7411@gmail.com>
2022-11-10 10:28:15 +0530

    addded mainRoutes

---
+++ meanbuy/src/pages/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Routes,Route} from "react-router-dom"
+import Login from './Login'
+import SignUp from './SignUp'
+const MainRoutes = () => {
+  return (
+    <Routes >
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/SignUp' element={<SignUp />}></Route>
+        <Route path="*" element={{}}/>
+    </Routes>
+  )
+}
+
+export default MainRoutes

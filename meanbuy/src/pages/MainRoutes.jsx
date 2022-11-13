7484d0d89493a6b938f8d389130fe4241020e8de
Anwar Basha <bashaa7411@gmail.com>
2022-11-13 11:29:09 +0530

    Update MainRoutes.jsx

--- meanbuy/src/pages/MainRoutes.jsx
+++ meanbuy/src/pages/MainRoutes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import SignUp from "./SignUp";
@@ -19,39 +19,37 @@ import DeliveryDetails from "../components/DeliveryDetails";
 import ThankYou from "../components/ThankYou";
 import Watchpage from "./Watchpage";
 import PrivateRoute from "./PrivateRoute";
+import PageNotFound from "./PageNotFound";
+import { Product } from "./Product";
 import FeedbackForm from "./FeedbackForm";
-const MainRoutes = () => {
-     return (
-          <Routes>
-               <Route path="/login" element={<Login />}></Route>
-               <Route path="/SignUp" element={<SignUp />}></Route>
-               <Route path="/home/electronics" element={<Electronics />} />
-               <Route path="/clothing" element={<Clothing /> }></Route>
-               <Route path="/" element={<Home />} />
-               <Route path="/FlashSale" element={<FlashSale />} />
-               <Route path="/BestDeal" element={<BestDeal />} />
-               <Route path="/Shop" element={<Shop />} />
-               <Route path="/Trending" element={<Trending />} />
 
-               <Route path="/Watchpage" element={<Watchpage />} />
-               <Route path="/admindashboard" element={<Admindashboard />} />
-               <Route
-                    path="/vendorRegistrationForm"
-                    element={<VendorRegistrationForm />}
-               />
-               <Route path="/marchentLogin" element={<MarchentLogin />} />
-               <Route
-                    path="/productUploadForm"
-                    element={<ProductUploadMarchent />}
-               />
-               <Route path="/checkout" element={<Checkout />} />
-               <Route path="/cardpayment" element={<CardPayment />} />
-               <Route path="/deliverydetails" element={<DeliveryDetails />} />
-               <Route path="/thankyou" element={<ThankYou />} />
-               <Route path="/FeedbackForm" element={<FeedbackForm />} />
-               <Route path="*" element={<div>Page Not Found!</div>} />
-          </Routes>
-     );
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/SignUp" element={<SignUp />}></Route>
+      <Route path="/electronics" element={<Electronics />} />
+      <Route path="/electronics/:id" element={<Product />} />
+      <Route path="/clothing" element={<Clothing />}></Route>
+      <Route path="/products/:id" element={<Product />} />
+      <Route path="/Watchpage" element={<Watchpage />} />
+      <Route path="/Watchpage/:id" element={<Product />} />
+      <Route path="/admindashboard" element={<Admindashboard />} />
+      <Route
+        path="/vendorRegistrationForm"
+        element={<VendorRegistrationForm />}
+      />
+      <Route path="/marchentLogin" element={<MarchentLogin />} />
+      <Route path="/productUploadForm" element={<ProductUploadMarchent />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/cardpayment" element={<CardPayment />} />
+      <Route path="/deliverydetails" element={<DeliveryDetails />} />
+      <Route path="/thankyou" element={<ThankYou />} />
+      <Route path="/FeedbackForm" element={<FeedbackForm />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
 };
 
 export default MainRoutes;
